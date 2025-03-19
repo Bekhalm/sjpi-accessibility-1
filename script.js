4343475d8c6daadf6cd036b0390b5dfa82e77ef5
bekhalm <bekhal.mohammadi@lerniastudent.se>
2025-03-19 21:32:31 +0100

    Improvements of the site

--- script.js
+++ script.js
@@ -1,34 +1,46 @@
+
+//INAKTIVERAT DET MESTA HÄR EFTERSOM DET INTE GJORDE SIDAN NÅGOT GOTT ÖVER HUVUDTAGET.
+
+
+
+
 // Simulerad blockering av tråden
-setInterval(() => {
-  let start = Date.now()
-  while (Date.now() - start < 1000) {}
-}, 3000)
+//setInterval(() => //!!  let start = Date.now()
+//  while (Date.now() - start < 1000) {}
+//}, 3000)
+
+
 
 // Skriver ut HTML direkt i dokumentet
-document.write("<h1 style='color:black;'>HELLO FROM DOCUMENT.WRITE()</h1>")
-document.write("<p>Another document.write() call</p>")
+//document.write("<h1 style='color:black;'>HELLO FROM DOCUMENT.WRITE()</h1>")
+//document.write("<p>Another document.write() call</p>")
+let div = document.createElement('div');
+div.innerHTML = "<h1 style='color:black;'>HELLO FROM DOCUMENT.WRITE()</h1><p>Another document.write() call</p>";
+div.style.display = 'none'; 
+document.body.appendChild(div);
 
 // Förhindrar användaren att navigera bakåt
-history.pushState(null, "", location.href)
-window.onpopstate = () => history.go(1)
+//history.pushState(null, "", location.href)
+//window.onpopstate = () => history.go(1)
 
 // Ändrar bakgrundsfärgen slumpmässigt var 500ms
-setInterval(() => {
-  document.body.style.backgroundColor =
-    "#" + Math.floor(Math.random() * 16777215).toString(16)
-}, 500)
+//setInterval(() => {
+ // document.body.style.backgroundColor =
+  //  "#" + Math.floor(Math.random() * 16777215).toString(16)
+//}, 500)
+
 
 // Visar innehållet på sidan efter 5 sekunder
-setTimeout(() => {
-  document.body.style.display = "block"
-}, 5000)
+//setTimeout(() => {
+//  document.body.style.display = "block"
+//}, 5000)
 
 // Loggar 10000 tal till konsolen efter 1 sekund
-setTimeout(() => {
-  for (let i = 0; i < 10000; i++) {
-    console.log(i)
-  }
-}, 1000)
+//setTimeout(() => {
+ // for (let i = 0; i < 10000; i++) {
+  //  console.log(i)
+  //}
+//}, 1000)
 
 // // Visar en alert när sidan har laddats
 // document.addEventListener("DOMContentLoaded", function () {
@@ -36,6 +48,6 @@ setTimeout(() => {
 // })
 
 // Loggar 100 000 siffror direkt när sidan laddas
-for (let i = 0; i < 100000; i++) {
-  console.log(i)
-}
+//for (let i = 0; i < 100000; i++) {
+ // console.log(i)
+//}
